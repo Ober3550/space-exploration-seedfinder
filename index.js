f29ac34f6d455ef817e418e009a77915dd44c24f
Oblivion <ober3550@gmail.com>
2024-05-28 21:36:03 +1000

    Added code to look for holm, cryo, vulc surfaces

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fs.readdirSync(dir).forEach((filename) => {
     filenames.push(dir + filename);
 });
 
-const filename = filenames[0];
+const filename = filenames[2];
 const file = readline.createInterface({
     input: fs.createReadStream(filename),
     output: process.stdout,
@@ -47,26 +47,37 @@ function rename(oldName) {
     return oldName;
 }
 
+function noColorString(string) {
+    return string.replace(/\x1b\[[0-9;]*m/g, "");
+}
+
 function noColorLength(string) {
-    return string.replace(/\x1b\[[0-9;]*m/g, "").length;
+    return noColorString(string).length;
+}
+
+function resourcesArray(resourcesObject) {
+    let resources = [];
+    let resourceKeys = Object.keys(resourcesObject).sort((a, b) => {
+        return resourcesObject[b] - resourcesObject[a];
+    });
+    for (let i = 0; i < Math.min(6, resourceKeys.length); i++) {
+        resources.push(resourceKeys[i]);
+        resources.push(Math.floor(resourcesObject[resourceKeys[i]] * 10000) / 10000);
+    }
+    return resources;
 }
 
 function surfaceInfo(surface) {
-    let resources = [];
-    let resourceKeys = Object.keys(surface.resource).sort((a, b) => {
-        return surface.resource[b] - surface.resource[a];
-    });
-    for (let i = 0; i < Math.min(6, resourceKeys.length); i++) {
-        resources.push(rename(resourceKeys[i]));
-        resources.push(Math.floor(surface.resource[resourceKeys[i]] * 10000) / 10000);
-    }
     return [
         surface.name,
+        surface.zone_type[0],
+        "dv",
         surface.delta_v,
-        surface.zone_type,
-        surface.tags.enemy,
-        surface.tags.water,
-        ...resources,
+        "r",
+        surface.radius,
+        surface.tags.enemy.replace("enemy_", "e ").replace("very_", "v"),
+        surface.tags.water.replace("water_", "w "),
+        ...resourcesArray(surface.resource).map((x) => rename(x)),
     ];
 }
 
@@ -96,19 +107,67 @@ function printTable(table) {
     }
 }
 
+function printPlanetsAndMoons(planetsAndMoons) {
+    let table = [];
+    for (let i = 0; i < planetsAndMoons.length; i++) {
+        table.push(surfaceInfo(planetsAndMoons[i]));
+    }
+    printTable(table);
+}
+
+const resourceNames = {
+    "se-vulcanite": true,
+    "se-cryonite": true,
+    "se-holmium-ore": true,
+    "se-beryllium-ore": true,
+    "se-iridium-ore": true,
+    "se-vitamelange": true,
+};
+function evalSeed(seedObject) {
+    let planets = seedObject.planets.sort((a, b) => {
+        return a.delta_v - b.delta_v;
+    });
+    let moons = seedObject.moons.sort((a, b) => {
+        return a.delta_v - b.delta_v;
+    });
+    let planetsAndMoons = [...planets, ...moons].sort((a, b) => {
+        return a.delta_v - b.delta_v;
+    });
+    // const distanceFilter = planets[3].delta_v;
+    // planetsAndMoons = planetsAndMoons.filter((surface) => {
+    //     return surface.delta_v <= distanceFilter;
+    // });
+    planetsAndMoons = planetsAndMoons.filter((surface) => {
+        let resources = resourcesArray(surface.resource);
+        return (
+            surface.tags.water != "water_none" &&
+            resourceNames[noColorString(resources[0])] &&
+            surface.radius > 2000
+        );
+    });
+    let specialHolm = planetsAndMoons.filter((surface) => {
+        let resources = resourcesArray(surface.resource);
+        return (
+            resources.includes("se-cryonite") &&
+            resources.includes("se-vulcanite") &&
+            resources.includes("se-holmium-ore")
+        );
+    });
+    if (specialHolm.length > 0) {
+        console.log(`seed: ${seedObject.seed} loot: ${seedObject.loot.join("")}`);
+        printPlanetsAndMoons(planetsAndMoons);
+    }
+    return null;
+}
+
 file.on("line", (line) => {
     let seedObject = JSON.parse(line);
     const modules = seedObject.loot.join("");
     if (modules.match(/^PPSS/)) {
-        console.log(`seed: ${seedObject.seed} modules: ${modules}`);
-        const planetsAndMoons = [...seedObject.moons, ...seedObject.planets].sort((a, b) => {
-            return a.delta_v - b.delta_v;
-        });
-        let table = [];
-        for (let i = 0; i < planetsAndMoons.length; i++) {
-            table.push(surfaceInfo(planetsAndMoons[i]));
+        let result = evalSeed(seedObject);
+        if (result) {
+            // Do something with a successful seed
         }
-        printTable(table);
-        process.exit(0);
+        // process.exit(0);
     }
 });
